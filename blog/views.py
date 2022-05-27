320aa2b1c04904cdd93292ed1d81520bef80ad23
Ezekiel Victor <codeepoch@gmail.com>
2022-05-27 12:04:47 +0100

    Added Templates and Stylesheet.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
+post = [
+    {
+        'author': 'Eazy',
+        'title': 'Test Post',
+        'content': 'I love to code',
+        'date_added': 'May 28, 2022',
+    },
+    {
+        'author': 'Emmy Sensei',
+        'title': 'What\'s for dinner',
+        'content': 'I\'m hungry',
+        'date_added': 'May 28, 2022',
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {'posts': post}
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
