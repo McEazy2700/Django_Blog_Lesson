d9f1aabb7307fe47302324f2e88c82b620d18dac
Ezekiel Victor <codeepoch@gmail.com>
2022-05-29 00:24:05 +0100

    Fixed the get absolute url bug.

--- blog/models.py
+++ blog/models.py
@@ -15,5 +15,5 @@ class Post(models.Model):
 
 
     def get_absolute_url(self):
-        return reverse("blog-detail", kwargs={"pk": self.pk})
+        return reverse("post-detail", kwargs={"pk": self.pk})
     
