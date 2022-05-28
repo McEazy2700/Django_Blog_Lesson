7d63929de0a92bf87f1aae456fe988f427b85836
Ezekiel Victor <codeepoch@gmail.com>
2022-05-28 22:59:59 +0100

    Added django_heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 env = environ.Env()
 environ.Env.read_env()
@@ -28,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('IS_DEVELOPMENT', True)
+DEBUG = env('IS_DEVELOPMENT', 'True')
 
 ALLOWED_HOSTS = [
     'techloaf.herokuapp.com'
@@ -165,3 +166,5 @@ AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+
+django_heroku.settings(locals())
